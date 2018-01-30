2c5a12c079e48540c5a4d88ec4349f240fd2ce53
AsianDave94 <Davidtjmalmberg@gmail.com>
2018-01-30 14:30:18 +0100

    BIl + MC

---
+++ ClassLibrary1/ClassLibrary1/Bil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Bil
+    {
+        public string Nummerplade
+        {
+            get { return Nummerplade; }
+            set { value = Nummerplade; }
+        } 
+        public DateTime Dato
+        {
+            get { return Dato; }
+            set { value = Dato; }
+        }
+        public int Pris()
+        {
+            return 240;
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/MC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class MC
+    {
+        public string Nummerplade
+        {
+            get { return Nummerplade; }
+            set { value = Nummerplade; }
+        }
+        public DateTime Dato
+        {
+            get { return Dato; }
+            set { value = Dato; }
+        }
+        public int Pris()
+        {
+            return 125;
+        }
+        public string Køretøj()
+        {
+            return "MC";
+        }
+    }
+}
